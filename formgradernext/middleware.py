163595d6c0ab47316c06bd403162f0e3b6322507
Greg Werner <werner.greg@gmail.com>
2021-11-05 11:26:57 -0400

    release: Bump to version 0.3.2 (#11)

--- formgradernext/middleware.py
+++ formgradernext/middleware.py
@@ -1,4 +1,6 @@
 from functools import wraps
+from typing import Dict
+
 from tornado.web import RequestHandler
 
 
@@ -6,11 +8,13 @@ def coop_coep_headers(f):
     """
     Sets the COOP and COEP headers, which are required for cross origin isolation (which unlocks
     certain features in embedded Starboard Notebook frames). These headers need to be present
-    all the way down (i.e. in the top level webpage, as well as )
+    all the way down (i.e. in the top level webpage, as well as pages within the iFrame).
     """
+
     @wraps(f)
-    def handle(self: RequestHandler, *args, **kwargs):
+    def handle(self: RequestHandler, *args, **kwargs) -> Dict:
         self.set_header("Cross-Origin-Embedder-Policy", "require-corp")
         self.set_header("Cross-Origin-Opener-Policy", "same-origin")
         return f(self, *args, **kwargs)
+
     return handle
--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import os
-from setuptools import setup, find_packages
 
-name = 'formgradernext'
+from setuptools import find_packages
+from setuptools import setup
+
+name = "formgradernext"
 here = os.path.abspath(os.path.dirname(__file__))
 version_ns = {}
-with open(os.path.join(here, name, '_version.py')) as f:
+with open(os.path.join(here, name, "_version.py")) as f:
     exec(f.read(), {}, version_ns)
 
 setup_args = dict(
     name=name,
-    version=version_ns['__version__'],
+    version=version_ns["__version__"],
     packages=find_packages(),
     install_requires=[
         "nbgrader>=0.6.2",
     ],
-    include_package_data=True
+    include_package_data=True,
 )
 
 if __name__ == "__main__":
